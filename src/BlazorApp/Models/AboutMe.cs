0146c10a315fdb10684baeac49f269db02a3bc11
Simone Fittipaldi <fittipaldi.s@hotmail.it>
2023-10-28 15:51:08 +0200

    Add favicon and bew prop in about class

--- src/BlazorApp/Models/AboutMe.cs
+++ src/BlazorApp/Models/AboutMe.cs
@@ -5,4 +5,5 @@ public class AboutMe
     public string Description { get; set; } = string.Empty;
     public List<string> Skills { get; set; } = new();
     public string DetailOrQuote { get; set; } = string.Empty;
+    public string AboutTitle { get; set; } = string.Empty;
 }

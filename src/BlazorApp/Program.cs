e36d32d1d0aff4ae7262452b1c9066aa9ae50bb1
Simone Fittipaldi <fittipaldi.s@hotmail.it>
2025-03-11 16:58:26 +0100

    Ok lacolizazione funziona

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp;
 using BlazorApp.Services;
+using System.Globalization;
+using BlazorApp.Shared.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddLocalization(options => options.ResourcesPath = "Shared/Resources");
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -13,6 +14,20 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<HeroImageService>();
 
+builder.Services.AddLocalization();
+var host = builder.Build();
+await host.SetDefaultCulture();
+await host.RunAsync();
+
+
 var app = builder.Build();
 
+var culture = new CultureInfo("de");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+
 await builder.Build().RunAsync();
+
+
+

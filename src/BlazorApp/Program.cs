f29ea1e273f49ddb016fdee2210045bd705c7fa7
Simone Fittipaldi <fittipaldi.s@hotmail.it>
2025-03-11 14:58:09 +0100

    ASDD LOCALIZATION

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -4,10 +4,15 @@ using BlazorApp;
 using BlazorApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddLocalization();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<HeroImageService>();
+
+var app = builder.Build();
 
 await builder.Build().RunAsync();
